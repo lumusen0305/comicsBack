5c550fe7ecaad4465194eef46ed10b1edfda51fa
lumusen0305 <lumusen890305@gmail.com>
2021-07-24 21:29:27 +0800

    finish list spider

--- app/comic/views.py
+++ app/comic/views.py
@@ -39,3 +39,22 @@ def getComic():
         update_time=item.find_all('span')
         rep["data"].append({'name':name,'url':url,'image':src,'star':star,'update_time':update_time[3].text[:14]})
     return Response(json.dumps(rep),  mimetype='application/json')
+
+@comic.route('/getComicList' , methods=['POST'])
+def getComicList():
+    params=request.json
+    comic_url=params["comic_url"]
+    rep = {
+    "data": [],
+    "code": 200,
+    "msg": "null"
+     }
+    r = requests.get(comic_url, headers = headers)
+    soup = BeautifulSoup(r.text, 'lxml')
+    comic_table = soup.find_all('div',id='chapter-list-1')
+    chapter_list=comic_table[0].find_all('ul')
+    for chapter in chapter_list:
+        chapter_detail=chapter.find_all('a',class_='status0')
+        for item in reversed(chapter_detail):
+            rep["data"].append({'chapter':item.get('title'),'url':'https://www.manhuagui.com'+item['href']})
+    return Response(json.dumps(rep),  mimetype='application/json')
--- test.py
+++ test.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import urllib
 import json
 import time
+from requests.api import get
 
 from sqlalchemy.sql.expression import null
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.98 Safari/537.36'}
@@ -11,19 +12,13 @@ rep = {
 "code": 200,
 "msg": "null"
  }
-link = 'https://www.manhuagui.com/list/view.html'
+link = 'https://www.manhuagui.com/comic/19430/'
 r = requests.get(link, headers = headers)
 soup = BeautifulSoup(r.text, 'lxml')
-comics = soup.find_all('ul',id="contList")
-comics_list= comics[0].find_all('li')
-for item in comics_list:
-    url = "https://www.manhuagui.com"+item.find('a')['href']
-    name=item.find('a')['title']
-    image=item.find_all('img')
-    if image[0].get('data-src')!=None:
-        src = image[0].get('data-src')
-    else:
-        src = image[0].get('src')
-    star=float(item.find('em').text)
-    update_time=item.find_all('span')
-    print(update_time[3].text[:14])
+comic_table = soup.find_all('div',id='chapter-list-1')
+chapter_list=comic_table[0].find_all('ul')
+for chapter in chapter_list:
+    chapter_detail=chapter.find_all('a',class_='status0')
+    for item in reversed(chapter_detail):
+        print(item.get('title'))
+        print('https://www.manhuagui.com'+item['href'])
